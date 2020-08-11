01eb6e185f3a8db9e464042c01bcc2863737135d
MattTerry <materry@gmail.com>
2020-08-11 00:19:15 -0400

    set stage to prod

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "7pjt7v00ijmqujf58orad0207l",     // CognitoClientID
-  "api_base_url": "https://v4llqhqtr2.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://v4llqhqtr2.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "coginto_hosted_domain": "mytodoappdemo-todo-stack.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d1a6sypddjko2w.amplifyapp.com"                                      // AmplifyURL
 };
